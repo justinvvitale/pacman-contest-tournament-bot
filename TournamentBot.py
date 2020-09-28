a4daec7e1bafbf3f8e28c73ca43e1728b2d1d4fc
Justin Vitale <4002351+justinvvitale@users.noreply.github.com>
2020-09-28 18:47:03 +1000

    Change over to bot, basic commands, announcement system and presence

--- TournamentBot.py
+++ TournamentBot.py
@@ -7,11 +7,13 @@ from time import strftime, localtime
 import discord
 import requests
 from bs4 import BeautifulSoup
+from discord.ext.commands import Bot
 
 SITE = "RETRACTED"  # Site pointing to tournament (Base URL)
 TOKEN = "RETRACTED"  # Discord token
 INTERVAL = 3600  # How often to check the site for tournaments
 AMOUNT_DISPLAY = 10  # How much of the leaderboard to display in the embed
+ADMIN = ["SOMENAME#1234"]  # Discord members who's allowed to make announcements
 
 
 # Helper functions
@@ -19,8 +21,8 @@ def updateAnnounceChannels():
     announceChannels.clear()
 
     # Fetching announce channels
-    guilds = client.guilds
-    for channel in client.get_all_channels():
+    guilds = bot.guilds
+    for channel in bot.get_all_channels():
         if "tournament" in str(channel.name) and channel.permissions_for(channel.guild.me).send_messages:
             announceChannels.append(channel)
             guilds.remove(channel.guild)
@@ -56,73 +58,108 @@ def getDefaultChannel(guild):
     return None
 
 
+async def pollAnnounce():
+    # Bypass (for testing) prints latest one
+    # tournaments.pop()
+
+    latestTournaments = list(set(fetchTournaments(requests.get(SITE))) - set(tournaments))
+
+    tournamentDifference = len(latestTournaments)
+
+    if tournamentDifference > 0:
+        print(str(tournamentDifference) + " new tournament(s) found, announcing...")
+        try:
+            for tournament in latestTournaments:
+                link = str(SITE + tournament['href'])
+
+                resultPage = requests.get(link)
+                leaderboard = fetchLeaderboard(resultPage)
+                configuration = fetchConfiguration(resultPage)
+
+                embed = discord.Embed(
+                    title=("Tournament (HISTORIC TEST) " + str(tournamentDifference + len(tournaments))),
+                    type="rich",
+                    url=link,
+                    description=("**"
+                                 + configuration[2].text
+                                 + "**\n```"
+                                 + leaderboard.iloc[:, [0, 1, 2]].to_string(index=False)
+                                 + "```\n"
+                                 + str(configuration[1].contents[0]))
+                )
+
+                # Send message
+                for announceChannel in announceChannels:
+                    try:
+                        await announceChannel.send(embed=embed)
+                    except:
+                        print("Error announcing to a channel")
+                        print(traceback.print_exc())
+                tournamentDifference -= 1
+
+            # Add tournaments to globally tracked
+            tournaments.extend(latestTournaments)
+
+        except:
+            print("Error announcing")
+            print(traceback.print_exc())
+
+
 # Global variables
 tournaments = fetchTournaments(requests.get(SITE))
 announceChannels = []
 
-client = discord.Client()
+bot = Bot(command_prefix='@')
+
+
+@bot.command()
+async def commands(ctx):
+    await ctx.send("**Tournament bot commands**\n"
+                   "> forcepoll - Force a tournament check"
+                   )
+
+
+@bot.command()
+async def forcepoll(ctx):
+    print("Force polling requested by " + str(ctx.author))
+    await ctx.send("Checking site for new tournaments...")
+    await pollAnnounce()
+
+
+@bot.command()
+async def announce(ctx, *, arg):
+    if str(ctx.author) not in ADMIN:
+        await ctx.send("You are not permitted to make announcements")
+        print("Unauthorised announce by " + str(ctx.author) + " with contents [" + str(arg) + "]")
+        return
+    print("Announcing [" + str(arg) + "] to all, requested by " + str(ctx.author))
+    await ctx.send("Announced")
+
+    # Announce it
+    for channel in announceChannels:
+        await channel.send(str(arg))
 
 
 async def backgroundTask():
     while True:
-        await client.wait_until_ready()
+        await bot.wait_until_ready()
         counter = 0
+
+        await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching,
+                                                            name="for tournaments"))
 
         updateAnnounceChannels()
 
-        print('Connected with {0.user}'.format(client))
+        print('Connected with {0.user}'.format(bot))
 
-        while not client.is_closed():
+        while not bot.is_closed():
             counter += 1
 
             # Check for tournament update
             print("Executing cycle(" + str(counter) + ") at [" + strftime("%d-%m-%Y- %H:%M %p", localtime()) + "] - "
-                  + str(len(client.guilds)) + " guilds and " + str(len(announceChannels)) + " channels")
+                  + str(len(bot.guilds)) + " guilds and " + str(len(announceChannels)) + " channels")
 
-            # Bypass (for testing) prints latest one
-            # tournaments.pop()
-
-            latestTournaments = list(set(fetchTournaments(requests.get(SITE))) - set(tournaments))
-
-            tournamentDifference = len(latestTournaments)
-
-            if tournamentDifference > 0:
-                print(str(tournamentDifference) + " new tournament(s) found, announcing...")
-                try:
-                    for tournament in latestTournaments:
-                        link = str(SITE + tournament['href'])
-
-                        resultPage = requests.get(link)
-                        leaderboard = fetchLeaderboard(resultPage)
-                        configuration = fetchConfiguration(resultPage)
-
-                        embed = discord.Embed(
-                            title=("Tournament (HISTORIC TEST) " + str(tournamentDifference + len(tournaments))),
-                            type="rich",
-                            url=link,
-                            description=("**"
-                                         + configuration[2].text
-                                         + "**\n```"
-                                         + leaderboard.iloc[:, [0, 1, 2]].to_string(index=False)
-                                         + "```\n"
-                                         + str(configuration[1].contents[0]))
-                        )
-
-                        # Send message
-                        for announceChannel in announceChannels:
-                            try:
-                                await announceChannel.send(embed=embed)
-                            except:
-                                print("Error announcing to a channel")
-                                print(traceback.print_exc())
-                        tournamentDifference -= 1
-
-                    # Add tournaments to globally tracked
-                    tournaments.extend(latestTournaments)
-
-                except:
-                    print("Error announcing")
-                    print(traceback.print_exc())
+            await pollAnnounce()
 
             print("Finished cycle(" + str(counter) + ")")
             await asyncio.sleep(INTERVAL)
@@ -130,35 +167,38 @@ async def backgroundTask():
         print("Client lost connection")
 
 
-@client.event
+@bot.event
 async def on_guild_join(guild):
     updateAnnounceChannels()
 
 
-@client.event
+@bot.event
 async def on_guild_remove(guild):
     updateAnnounceChannels()
 
 
-@client.event
+@bot.event
 async def on_guild_channel_update(before, after):
     updateAnnounceChannels()
 
 
-@client.event
+@bot.event
 async def on_guild_channel_delete(channel):
     updateAnnounceChannels()
 
 
-@client.event
+@bot.event
 async def on_guild_channel_create(channel):
     updateAnnounceChannels()
 
 
-@client.event
+@bot.event
 async def on_guild_role_update(before, after):
     updateAnnounceChannels()
 
 
-client.loop.create_task(backgroundTask())
-client.run(TOKEN)
+# Loops
+bot.loop.create_task(backgroundTask())
+
+# Start
+bot.run(TOKEN)
